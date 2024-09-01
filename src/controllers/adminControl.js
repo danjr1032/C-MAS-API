30e081f6906fedf1e912f7d7e88467b09d3ca0a2
danjr1032 <tapjidan@gmail.com>
2024-09-01 13:33:28 +0100

    update to delete feedback 1

--- src/controllers/adminControl.js
+++ src/controllers/adminControl.js
@@ -308,7 +308,8 @@ exports.deleteFeedback = async (req, res) => {
       return res.status(404).json({ error: 'feedback not found' });
     }
 
-    res.status(200).json({ message: 'feedback deleted successfully' });
+    res.status(200).json({ success: true, message: 'feedback deleted successfully' });
+
   } catch (error) {
     res.status(500).json({ error: 'An error occurred while deleting feedback' });
   }
